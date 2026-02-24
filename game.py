32b2112d487acd1d8577c5455131ced728cc0583
LittleJoee <josephowolabi428@gmail.com>
2026-02-24 22:24:49 +0100

    player jump

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from scripts.util import load_image, load_images
+from scripts.entities import Player
 
 class Game:
     def __init__(self):
@@ -11,45 +12,43 @@ class Game:
         pygame.display.set_caption("dino game")
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
+        self.clock = pygame.time.Clock()
 
         self.movement = [False, False]
 
         self.assets = {
-            'player/idle' : load_image('01')
+            'player/idle' : load_image('player/idle/01.png')
         }
+
+        self.player = Player(self, [50, 50], [32, 18])
 
         self.x_scroll = 0
 
     def run(self):
         while True:
-            self.display.fill((0, 0, 0))
+            self.display.fill((255, 255, 255))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.type == pygame.K_SPACE:
-                        self.movement[0] = True
-                    if event.type == pygame.K_UP:
-                        self.movement[0] = True
-                    if event.type == pygame.K_w:
-                        self.movement[0] == True
-                    if event.type == pygame.K_DOWN:
-                        self.movement[1] = True
-                    if event.type == pygame.K_s:
-                        self.movement[1] == True
-                if event.type == pygame.KEYUP:
                     if event.key == pygame.K_SPACE:
+                        self.player.jump()
+                    if event.key == pygame.K_UP:
+                       self.player.jump()
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.type == pygame.K_UP:
-                        self.movement[0] = False
-                    if event.type == pygame.K_w:
-                        self.movement[0] == False
-                    if event.type == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-                    if event.type == pygame.K_s:
-                        self.movement[1] == False
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
--- scripts/entities.py
+++ scripts/entities.py
@@ -1,3 +1,62 @@
+import pygame
+
 class Entity:
-    def __init__(self, game, e_type, pos):
-        pass
+    def __init__(self, game, e_type, pos, size):
+        self.game = game
+        self.e_type = e_type
+        self.pos = pos
+        self.size = size
+        self.velocity = [0, 0]
+        self.collisions = {'down': False}
+
+        self.action = ''
+        self.anim_offset = (0, 0)
+
+    def update(self, y_floor=200, movement=(0, 0)):
+
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
+        if self.pos[1] >= y_floor:
+            self.collisions['down'] = True
+        else:
+            self.collisions['down'] = False
+
+        self.pos[0] += frame_movement[0]
+        self.pos[1] += frame_movement[1]
+
+        if self.pos[1] >= y_floor:
+            self.pos[1] = y_floor
+
+        if self.collisions['down']:
+            self.velocity[0] = 0
+
+    def set_action(self, action):
+        if self.action != action:
+            self.action = action
+            self.animation = self.game.asset[self.e_type + '/' + self.action].copy()
+
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+    def render(self, surf):
+        surf.blit(self.game.assets['player/idle'], (self.pos[0], self.pos[1]))
+
+
+class Player(Entity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'player', pos, size)
+        self.can_jump = True
+
+    def update(self, movement, y_floor=200):
+        super().update(movement=movement)
+        if self.collisions['down'] == True:
+            self.can_jump = True
+        else:
+            self.can_jump = False
+        
+    def jump(self):
+        if self.can_jump:
+            print(True)
+            self.velocity[1] = -3
